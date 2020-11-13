f80b95d25c774819409b8626bf8af8cef651ea83
zacwatts <zdwatts@gmail.com>
2020-11-13 11:38:35 -0700

    implemented createTask method

--- src/AppWithContext.js
+++ src/AppWithContext.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import TodoContext from './contexts/TodoContext';
+import App from './App';
 
 class AppWithContext extends React.Component {
     constructor() {
         super();
         const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+        this.state = {
+            tasks: storedTasks || {},
+            createTask: this.createTask,
+            deleteTask: this.deleteTask,
+        }
     }
 
     createTask = (task) => {
-
-    }
+        const date = new Date();
+        const nextTaskId = (date).getTime();
+        const newTask = {
+            [nextTaskId]: {
+                id: nextTaskId,
+                message: task,
+            }
+        }
+        this.setState((state, props) => ({
+            tasks: { ...state.tasks, ...newTask },
+        }), () => this.updateLocalStorageTasks());
+    };
 
     deleteTask = (id) => {
 
     }
 
-    updateLocalStorageTask = () => {
+    updateLocalStorageTasks = () => {
         console.log(this.state.task)
         const jsonTasks = JSON.stringify(this.state.tasks);
         localStorage.setItem("tasks", jsonTasks)
